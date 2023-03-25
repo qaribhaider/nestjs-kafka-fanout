941ab270fa30f1bc7c19e7381e98af04e8dc90fa
Syed Qarib <qaribhaider@gmail.com>
2023-03-25 17:27:01 +0100

    Added tests for api-gateway

--- apps/api-gateway/src/app.controller.spec.ts
+++ apps/api-gateway/src/app.controller.spec.ts
@@ -1,22 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientKafka } from '@nestjs/microservices';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
+  let mockClient: jest.Mocked<ClientKafka>;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    // Mock the `Math.random` function to always return 0.5
+    jest.spyOn(Math, 'random').mockReturnValue(0.5);
+
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: 'api_gateway_app',
+          useFactory: () => ({
+            emit: jest.fn(),
+          }),
+        },
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    // Get the instance of the controller, service, and Kafka client mock
+    appController = moduleRef.get<AppController>(AppController);
+    appService = moduleRef.get<AppService>(AppService);
+    mockClient = moduleRef.get('api_gateway_app');
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+  afterEach(() => {
+    // Restore the original implementation of the `Math.random` function
+    jest.restoreAllMocks();
+  });
+
+  it('should emit message to topic_01', async () => {
+    // Define a mock message with a fixed `rand` property value
+    const mockMsg = {
+      item_01: 'here is a test val',
+      rand: 500, // use a fixed value instead of Math.random
+      date: new Date().toString(),
+    };
+
+    // Call the test function on the controller
+    await appController.testKafka();
+
+    // Verify that the Kafka client mock's `emit` function was called with the correct arguments
+    expect(mockClient.emit).toHaveBeenCalledWith('topic_01', mockMsg);
   });
 });
--- apps/api-gateway/test/app.e2e-spec.ts
+++ apps/api-gateway/test/app.e2e-spec.ts
@@ -1,12 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: any;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +14,28 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/kafka-test (GET)', async () => {
+    jest.setTimeout(30000);
+
+    const res = await request(app.getHttpServer()).get('/kafka-test');
+
+    // Verify that the response status code is 200 OK
+    expect(res.status).toBe(200);
+
+    // Verify that the response body has the expected structure and values
+    expect(res.body).toEqual([
+      {
+        topicName: expect.any(String),
+        partition: expect.any(Number),
+        errorCode: 0,
+        baseOffset: expect.any(String),
+        logAppendTime: expect.any(String),
+        logStartOffset: expect.any(String),
+      },
+    ]);
   });
 });
